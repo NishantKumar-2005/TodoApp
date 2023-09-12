2d6e418ce6b247002c4302418612ab3eec5281e4
NishantKumar-2005 <120775351+NishantKumar-2005@users.noreply.github.com>
2023-09-12 21:04:49 +0530

    updated

--- util/feature.js
+++ util/feature.js
@@ -1,14 +1,9 @@
-import Jwt from "jsonwebtoken";
+import Jwt  from "jsonwebtoken";
 
-export const cookiesSend = (res, User, message, statusCode = 200) => {
-  const token = Jwt.sign({ _id: User._id }, process.env.JWT_SECRET);
+export const cookiesSend=(res,User,message,statusCode=200)=>{
+    const token=Jwt.sign({_id:User._id},process.env.JWt_secert);
 
-  console.log(token);
-  res.cookie("token", token, {
-    httpOnly: true,
-    maxAge: 15 * 60 * 1000,
-    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-    secure: process.env.NODE_ENV === "production", // Set to true in production (HTTPS)
-  });
-  res.status(statusCode).json({ success: true, message });
-};
+    console.log(token);
+    res.cookie("token",token,{httpOnly:true,maxAge:15*60*1000,sameSite:process.env.Node_ENV==="Development"?"lax":"none",secure: process.env.NODE_ENV === "production"?true:false});
+    res.status(statusCode).json({success:true,message});
+}
